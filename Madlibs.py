02ac4a28c6483f4a372b1b70bba2f060cbe3c71b
Astois! <159559194+Astois@users.noreply.github.com>
2024-02-09 19:08:50 +0000

    Create Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,6 @@
+def main():
+    Noun = input("enter a noun ")
+    Adjective = input("enter a Adjective ")
+    Verb = input("Enter a verb ")
+    print(f"once upon a time, there was a {Adjective} {Noun} named Sparky. He loved to {Verb} squirrels in the park.")
+main()
